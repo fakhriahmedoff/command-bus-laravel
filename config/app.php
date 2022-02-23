cb5b328dd7bacc86519e721ae3a6fc83baab4680
fakhriahmadov <fakhriahmedoff@gmail.com>
2022-02-23 11:00:56 +0400

    done  boilerplate

--- config/app.php
+++ config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Skyronic\FileGenerator\FileGeneratorServiceProvider::class,
+
 
         /*
          * Package Service Providers...
